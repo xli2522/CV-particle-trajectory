194cccf4e7fd60c639e95b1cbab4be942ef40561
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-06-24 05:50:19 -0400

    quickFeaturePosition testing w/ testing video

--- trackCell_simple.py
+++ trackCell_simple.py
@@ -1,6 +1,8 @@
 # the following code assumes low frame numbers (~20,000); 
 # --> simplicity > efficiency (speed); 
 # may be optimized for higher frame numbers
+from __future__ import print_function
+from operator import index
 
 #CV and Math
 import cv2                      #openCV; Powerful computer vision
@@ -12,6 +14,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series  
+import argparse
 
 #Utilities
 import time
@@ -22,14 +25,19 @@ import glob
 from pathlib import Path
 import shutil
 
+from trackpy.plots import make_axes
+
 #set styles
 mpl.rc('figure',  figsize=(10, 5))
 mpl.rc('image', cmap='gray')
 
 def main():
     '''Main testing function'''
-    processedVideo, framesPath = track.prepareVideo('video\concave1.mp4', savePic=True, skip=5)
-    track.trackProcessedVideo(str(processedVideo))
+    #processedVideo, framesPath = track.prepareVideo('video\concave1.mp4', savePic=True, skip=5)
+    track.trackProcessedVideo('1624505972.8145204grayscale_processed.avi')
+    #track.identify('frames/0.png')
+    #noted = track.quickFeaturePosition('frames/0.png', inspect=True)
+
     return
 
 class track:
@@ -93,7 +101,7 @@ class track:
 
         return processedVideoTitle, savePath
 
-    def trackProcessedVideo(video):
+    def trackProcessedVideo(video, savePic=False):
         '''Function performs cell tracking and position logging
         Imput:
                 video               (string)            video file location
@@ -112,15 +120,31 @@ class track:
         print('FPS: '+str(fps))
 
         print('Inspect feature selection...')
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)        # $$ FIX $$ correct grayscale parameter in cv2, remove this line
-        track.identify(frame)                                   # identify cells of interest present in frame 0
+        frame0 = frame.copy()
+        frame0 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)        # $$ FIX $$ correct grayscale parameter in cv2, remove this line
+        track.identify(frame0)                                   # identify cells of interest present in frame 0
 
-        user = input('y to continue; any key to quit program...')           #continue if satisfied with cell selection
-        if user != 'y':
-            raise Exception('Program interrupted by user...')               #interrupt if not
+        #user = input('y to continue; any key to quit program...')           #continue if satisfied with cell selection
+        #if user != 'y':
+            #raise Exception('Program interrupted by user...')               #interrupt if not
+        savePath = Path('notedframes/')      
+        savePath.mkdir(exist_ok=True)       #make directory frames/ under the current parent directory for savePic
 
         for i in tqdm(range(length)):
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            labeledFrame = track.quickFeaturePosition(frame)
+            cv2.imwrite(os.path.join(savePath, str(i) + '.png'), labeledFrame)             #save processed frames and make video
             ret, frame = cap.read()             #go through each frame
+
+        notedName = 'noted_'+str(video)
+        imv.local.timelapse(str(notedName), 15,  str(savePath))       #construct video from processed frames $$ FIX $$ grayscale in cv2
+        
+        if not savePic:                                         
+            try:                                                #trash the picture directory 
+                shutil.rmtree(savePath)
+
+            except OSError as e:
+                print(f'Error: {savePath} : {e.strerror}')
 
         return
     
@@ -152,13 +176,65 @@ class track:
         return
 
     def identify(frame):
-        '''Dummy Testing Function'''
-        f = tp.locate(frame, 51, invert=True)
-        f.head()
-        tp.annotate(f, frame)
+        '''Identify features in a frame'''
 
+        if isinstance(frame, str):
+            frame = cv2.imread(str(frame), cv2.IMREAD_GRAYSCALE)
+        f = tp.locate(frame, 21, percentile=99, invert=True)
+        f.head()
+        tp.annotate(f, frame, color='k')
+        
         return 
 
+    def quickFeaturePosition(frame, inspect=False, method=0):
+        '''Testing cv2 quick feature detection'''
+        if method == 0:
+            ######## trackpy locate ########
+            if isinstance(frame, str):
+                frame = cv2.imread(str(frame), cv2.IMREAD_GRAYSCALE)
+            f = tp.locate(frame, 21, percentile=99, invert=True)
+
+            notedFrame = track.stealthAnnotate(f, frame)
+            if inspect:
+                cv2.imshow('inspect',notedFrame)
+                cv2.waitKey(0)
+        else:
+            ############ CV2 ORB ############
+            '''
+            img = cv2.imread(str(frame),0)
+            # Initiate ORB detector
+            orb = cv2.ORB_create()
+            # find the keypoints with ORB
+            kp = orb.detect(img,None)
+            # compute the descriptors with ORB
+            kp, des = orb.compute(img, kp)
+            # draw only keypoints location,not size and orientation
+            img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+            plt.imshow(img2), plt.show()'''
+            pass
+
+        return notedFrame
+
+    def simpleMask(frame):
+        '''Simplify frames further into B&W frames'''
+        
+        return
+
+    def batchIdentify(frame):
+        '''Optimization and linking'''
+
+        return
+
+    def stealthAnnotate(centroids, image, circle_size=21, color=0, thickness=2):
+        '''custom annotation'''
+        positionX, positionY = centroids['x'].tolist(), centroids['y'].tolist()
+    
+        for i in range(len(positionX)):
+            #position approximated to the nearest integer for drawing
+            image = cv2.circle(image, (int(positionX[i]), int(positionY[i])), circle_size, color, thickness)
+        
+        return image
+         
 class analysis:
 
     def meanPosition():
